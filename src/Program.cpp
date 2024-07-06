417c6a91330df6cdbf805337c2dd574ae0f3f9e4
TinyProbe <137adv167@gmail.com>
2024-07-06 18:07:36 +0900

    update

--- src/Program.cpp
+++ src/Program.cpp
@@ -15,10 +15,14 @@ Program::~Program() {
 }
 
 int Program::run() {
-  SDL_Color const green = { 0, 255, 0, 255 };
-  SDL_Color const purple = { 255, 0, 255, 255 };
+  std::vector<SDL_Color> const colors = {
+    { 255, 0, 0, 255 },
+    { 0, 255, 0, 255 },
+    { 0, 0, 255, 255 },
+  };
   int const wid = 640;
   int const hei = 480;
+  int const refresh_time = 200;
   SDL_Window *p_window = nullptr;
   SDL_Renderer *p_renderer = nullptr;
   if (SDL_CreateWindowAndRenderer(wid, hei, 0, &p_window, &p_renderer) < 0) {
@@ -26,12 +30,13 @@ int Program::run() {
     return -1;
   }
   SDL_SetWindowTitle(p_window, "Screen flip example");
-  int lastTickCount = SDL_GetTicks();
-  int curTickCount = lastTickCount;
+
+  int last_tick_count = SDL_GetTicks();
+  int cur_tick_count = last_tick_count;
   int color = 0;
   bool running = true;
+  SDL_Event event;
   while (running) {
-    SDL_Event event;
     while (SDL_PollEvent(&event)) {
       if (event.type == SDL_QUIT) {
         running = false;
@@ -41,21 +46,19 @@ int Program::run() {
         }
       }
     }
-    curTickCount = SDL_GetTicks();
-    if (curTickCount - lastTickCount > 100) {
-      ++color %= 2;
-      lastTickCount = curTickCount;
+    cur_tick_count = SDL_GetTicks();
+    if (cur_tick_count - last_tick_count >= refresh_time) {
+      ++color %= (int)colors.size();
+      SDL_SetRenderDrawColor(p_renderer,
+                             colors[color].r,
+                             colors[color].g,
+                             colors[color].b,
+                             colors[color].a);
+      SDL_RenderClear(p_renderer);
+      SDL_RenderFillRect(p_renderer, NULL);
+      SDL_RenderPresent(p_renderer);
+      last_tick_count = cur_tick_count;
     }
-    if (color == 0) {
-      SDL_SetRenderDrawColor(
-          p_renderer, green.r, green.g, green.b, green.a);
-    } else {
-      SDL_SetRenderDrawColor(
-          p_renderer, purple.r, purple.g, purple.b, purple.a);
-    }
-    SDL_RenderClear(p_renderer);
-    SDL_RenderFillRect(p_renderer, NULL);
-    SDL_RenderPresent(p_renderer);
   }
   SDL_DestroyRenderer(p_renderer);
   SDL_DestroyWindow(p_window);
